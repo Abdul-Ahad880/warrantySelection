8a65ebc2856058a8b7089cccd912301c0db9d942
Abdul-Ahad880 <154658322+Abdul-Ahad880@users.noreply.github.com>
2025-05-23 11:25:37 +0500

    add placeholder comment in index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-
+///
 app.use("/api/expanded", expandedRoutes);
 app.use("/api/master", masterRoutes);
 app.use("/api/system", systemRoutes);
